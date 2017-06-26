ed0a4d141abfab934b11311d15379b49dcc5db98
Sean Howard <snhoward@fb.com>
2017-06-26 14:00:28 -0700

    created an item_tweet layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -12,7 +12,7 @@ public class Tweet {
     // list of attributes
     public String body;
     public long uid; // database ID for tweet
-    // public User user;
+    public User user;
     public String createdAt;
 
     // deserialize the data
@@ -23,6 +23,7 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.uid = jsonObject.getLong("id");
         tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
         return tweet;
     }
 
